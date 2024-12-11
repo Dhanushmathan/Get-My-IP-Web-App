b02495714fd216e78195e5d09035b628abb88720
Dhanushmathan <lovelymathan2394@gmail.com>
2024-12-11 15:19:24 +0530

    feat: get IP adress app build API

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-// Write your javascript code here
+// Get the ip address for this url: https://ipv4.icanhazip.com/
+const ipAddressPlaceholder = document.querySelector("#ipAddress");
+const xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        ipAddressPlaceholder.textContent = xhr.responseText;
+    }
+};
+
+xhr.open("GET", "https://ipv4.icanhazip.com/", true)
+xhr.send()
